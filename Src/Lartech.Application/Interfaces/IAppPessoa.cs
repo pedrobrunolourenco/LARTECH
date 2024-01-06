bb06d1f6e9b662d708061834c6d6c5a3098f7c0b
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-01-06 05:35:22 -0300

    COMMIT-008

---
+++ Src/Lartech.Application/Interfaces/IAppPessoa.cs
@@ -0,0 +1,28 @@
+﻿using Lartech.Application.Models;
+using Lartech.Domain.Entidades;
+
+namespace Lartech.Application.Interfaces
+{
+    public interface IAppPessoa
+    {
+        IEnumerable<PessoaModel> ObterTodas();
+        PessoaModel? ObterPorId(Guid id);
+        PessoaModel? ObterPorCpf(string cpf);
+        IEnumerable<PessoaModel> ObterPorParteDoNome(string nome);
+
+        IEnumerable<PessoaModel> ObterAtivos();
+        IEnumerable<PessoaModel> ObterInativos();
+
+        PessoaModel IncluirPessoa(PessoaModel pessoa);
+        PessoaModel AlterarPessoa(PessoaModel pessoa);
+        void ExcluirPessoa(Guid id);
+
+        TelefoneModel AdicionarTelefone(TelefoneModel fone);
+        TelefoneModel AlterarTelefone(TelefoneModel fone);
+        void ExcluirTelefone(Guid idtelefone);
+
+        PessoaModel Ativar(PessoaModel pessoa);
+        PessoaModel Inativar(PessoaModel pessoa);
+
+    }
+}
---
+++ Src/Lartech.Application/Services/AppPessoa.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using Lartech.Application.Interfaces;
+using Lartech.Application.Models;
+using Lartech.Domain.Entidades;
+using Lartech.Domain.Interfaces.Service;
+
+namespace Lartech.Application.Services
+{
+    public class AppPessoa : IAppPessoa
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IServicePessoa _servicePessoa;
+
+        public AppPessoa(IMapper mapper,
+                         IServicePessoa servicePessoa)
+        {
+            _mapper = mapper;
+            _servicePessoa = servicePessoa;
+        }
+
+        public IEnumerable<PessoaModel> ObterTodas()
+        {
+            return _mapper.Map<IEnumerable<PessoaModel>>(_servicePessoa.ObterTodas());
+        }
+
+        public IEnumerable<PessoaModel> ObterAtivos()
+        {
+            return _mapper.Map<IEnumerable<PessoaModel>>(_servicePessoa.ObterAtivos());
+        }
+
+        public IEnumerable<PessoaModel> ObterInativos()
+        {
+            return _mapper.Map<IEnumerable<PessoaModel>>(_servicePessoa.ObterInativos());
+        }
+        public PessoaModel? ObterPorId(Guid id)
+        {
+            return _mapper.Map<PessoaModel>(_servicePessoa.ObterPorId(id));
+        }
+
+        public PessoaModel? ObterPorCpf(string cpf)
+        {
+            return _mapper.Map<PessoaModel>(_servicePessoa.ObterPorCpf(cpf));
+        }
+
+        public IEnumerable<PessoaModel> ObterPorParteDoNome(string nome)
+        {
+            return _mapper.Map<IEnumerable<PessoaModel>>(_servicePessoa.ObterPorParteDoNome(nome));
+        }
+
+
+        public PessoaModel IncluirPessoa(PessoaModel pessoa)
+        {
+            return _mapper.Map<PessoaModel>(_servicePessoa.IncluirPessoa(_mapper.Map<Pessoa>(pessoa)));
+        }
+        public PessoaModel AlterarPessoa(PessoaModel pessoa)
+        {
+            return _mapper.Map<PessoaModel>(_servicePessoa.AlterarPessoa(_mapper.Map<Pessoa>(pessoa)));
+        }
+
+        public void ExcluirPessoa(Guid id)
+        {
+            _servicePessoa.ExcluirPessoa(id);
+        }
+
+
+        public TelefoneModel AdicionarTelefone(TelefoneModel fone)
+        {
+            return _mapper.Map<TelefoneModel>(_servicePessoa.AdicionarTelefone(_mapper.Map<Telefone>(fone)));
+        }
+
+        public TelefoneModel AlterarTelefone(TelefoneModel fone)
+        {
+            return _mapper.Map<TelefoneModel>(_servicePessoa.AlterarTelefone(_mapper.Map<Telefone>(fone)));
+        }
+        public void ExcluirTelefone(Guid idtelefone)
+        {
+            _servicePessoa.ExcluirTelefone(idtelefone);
+        }
+
+        public PessoaModel Ativar(PessoaModel pessoa)
+        {
+            return _mapper.Map<PessoaModel>(_servicePessoa.Ativar(_mapper.Map<Pessoa>(pessoa)));
+        }
+
+        public PessoaModel Inativar(PessoaModel pessoa)
+        {
+            return _mapper.Map<PessoaModel>(_servicePessoa.Inativar(_mapper.Map<Pessoa>(pessoa)));
+        }
+
+
+
+
+    }
+}
