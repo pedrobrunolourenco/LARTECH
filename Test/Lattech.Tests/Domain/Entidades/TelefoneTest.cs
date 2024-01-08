cb8449fb752a57bb1ee7aea51267b56a40d54393
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-01-08 11:38:51 -0300

    COMMIT-041

--- Test/Lattech.Tests/Domain/Entidades/TelefoneTest.cs
+++ Test/Lattech.Tests/Domain/Entidades/TelefoneTest.cs
@@ -1,9 +1,4 @@
 ﻿using Lartech.Domain.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lattech.Tests.Domain.Entidades
 {
